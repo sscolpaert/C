9f7c8bc0bdb8a00e1d8e5996eeb1dba8e23e6c2b
Samuel <samuelcolpaert1@outlook.com>
2023-09-29 04:28:56 -0300

    1.0

---
+++ 3-Strings/Strings_3.c
@@ -0,0 +1,23 @@
+//Neste programa é feita a contagem de todas as vogais existentes em uma string de tamanho 30. Por fim, é mostrada a quantidade de vogais.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+main() {
+    // declaração da string
+    char palavra [30];
+    int i= 0, qtde=0;
+    // leitura da string
+    printf ("*** Digite a string ***\n");
+    gets (palavra);
+    //laço para percorrer a string
+    while (palavra [i] != '\0'){
+           if (palavra [i] == 'a' || palavra [i] == 'e' || palavra [i] == 'i' || palavra [i] == 'o' || palavra [i] == 'u') 
+           {   
+                 qtde++;         
+           }
+           i++;
+    }
+    printf ("\n A quantidade de vogais da palavra e = %d\n", qtde);
+    system ("PAUSE");
+}
---
+++ 3-Strings/Strings_4.c
@@ -0,0 +1,13 @@
+// Neste programa, a letra “a” é substituída pela letra “i” em uma string de tamanho 15. Por fim, a string alterada é exibida.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+main(){
+    //declaração da string
+    char palavra[15];
+    int i, tam;
+    //leitura da string
+    printf ("*** Digite a string ***")
+}
